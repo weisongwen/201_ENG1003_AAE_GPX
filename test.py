9ede115b7fd9386e8361567a7087a745abb0ab99
weisongwen <wenwsrobo@gmail.com>
2020-10-07 09:56:07 +0800

    uncomments some code

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ print("hello world")
 
 print("hello, my name is Weisong Wen")
 
-#a = 20
-#b = 100
-#c = a * b
-#print("print c value:")
-#print(c)
+a = 20
+b = 100
+c = a * b
+print("print c value:")
+print(c)
 
 def multiplication(a, b):
     c = a * b + 10
