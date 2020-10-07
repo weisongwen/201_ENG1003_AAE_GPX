a0cce62a7fc4742c5136064b714846cd337f9bcd
weisongwen <wenwsrobo@gmail.com>
2020-10-07 09:54:40 +0800

    add simple python test code

---
+++ test.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+print("hello, my name is Weisong Wen")
+
+#a = 20
+#b = 100
+#c = a * b
+#print("print c value:")
+#print(c)
+
+def multiplication(a, b):
+    c = a * b + 10
+    print("print the value from function multiplication")
+    print(c)
+
+multiplication(10, 8)
+
+
+
